a0d062722d6742e7f7822b9a5e5ba9ba1962e515
YuWei Chou <demoyuw@gmail.com>
2017-08-04 13:53:52 +0800

    Openstack control node restart neutron service.

---
+++ restart_neutron_service.sh
@@ -0,0 +1,6 @@
+service neutron-dhcp-agent restart
+service neutron-l3-agent restart
+service neutron-lbaasv2-agent restart
+service neutron-linuxbridge-agent restart
+service neutron-metadata-agent restart
+service neutron-server restart
