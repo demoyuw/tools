ebe634a6268782f1381e67d900e7ccbf3586d343
YuWei Chou <demoyuw@gmail.com>
2017-08-04 13:53:52 +0800

    Openstack compute node restart neutron service.

---
+++ status_neutron_service.sh
@@ -0,0 +1,6 @@
+service neutron-dhcp-agent status
+service neutron-l3-agent status
+service neutron-lbaasv2-agent status
+service neutron-linuxbridge-agent status
+service neutron-metadata-agent status
+service neutron-server status
