a2669c5dabc9a22a0597241871199218a324141d
YuWei Chou <demoyuw@gmail.com>
2017-08-09 14:10:55 +0800

    add install neutron service

--- install_neutron_service.sh
+++ install_neutron_service.sh
@@ -5,3 +5,6 @@ apt-get install -y neutron-dhcp-agent
 apt-get install -y neutron-l3-agent
 apt-get install -y neutron-linuxbridge-agent
 apt-get install -y neutron-metadata-agent
+apt-get install -y neutron-lbaasv2-agent 
+
+cp -pr /etc/neutron 
