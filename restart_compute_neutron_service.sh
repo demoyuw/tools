6dfd79c6bfb24179b946d0523b2918bdccc4de4c
YuWei Chou <demoyuw@gmail.com>
2017-10-03 14:06:00 +0800

    add restart_compute_neutron_service.sh

---
+++ restart_compute_neutron_service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+service neutron-linuxbridge-agent restart
+service neutron-linuxbridge-agent status
---
+++ restart_network_neutron_service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+service neutron-dhcp-agent restart
+service neutron-l3-agent restart
+service neutron-lbaasv2-agent restart
+service neutron-linuxbridge-agent restart
+service neutron-metadata-agent restart
+service neutron-server restart
+
+service neutron-dhcp-agent status
+service neutron-l3-agent status
+service neutron-lbaasv2-agent status
+service neutron-linuxbridge-agent status
+service neutron-metadata-agent status
+service neutron-server status
