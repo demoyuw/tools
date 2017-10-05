6c1a8b0250febe2dc9e528955f215ad9518e0a73
YuWei Chou <demoyuw@gmail.com>
2017-10-05 16:28:08 +0800

    add control neutron service restart, network neutron service restart

--- restart_control&network_neutron_service.sh
+++ restart_control&network_neutron_service.sh
@@ -5,9 +5,11 @@ service neutron-l3-agent restart
 service neutron-lbaasv2-agent restart
 service neutron-linuxbridge-agent restart
 service neutron-metadata-agent restart
+service neutron-server restart
 
 service neutron-dhcp-agent status
 service neutron-l3-agent status
 service neutron-lbaasv2-agent status
 service neutron-linuxbridge-agent status
 service neutron-metadata-agent status
+service neutron-server status
