4e767c904c36f1fa5d69a7e6bf99c3b2b3fd129a
YuWei Chou <demoyuw@gmail.com>
2017-12-12 15:06:45 +0800

    network interface bonding on ubuntu14 /etc/network/interfaces

--- network/bond_net_interface.sh
+++ network/bond_net_interface.sh
@@ -13,13 +13,13 @@ if [ $# -lt 6 ] ; then
     exit -1
 fi
 
-bond_interface=$1
+BOND_INTERFACE=$1
 IP=$2
-Netmask=$3
+NETMASK=$3
 GW=$4
-net_interface1=$5
-net_interface2=$6
-bond_mode=$7
+NET_INTERFACE1=$5
+NET_INTERFACE2=$6
+BOND_MODE=$7
 
 ping -c1 -W1 8.8.8.8 &> /dev/null
 if [ "$?" == "0" ]; then
@@ -30,32 +30,34 @@ if [ "$?" == "0" ]; then
     fi
 fi
 
-result=$(cat /etc/modules | grep 'bonding mode=`bond_mode` miimon=100')
+result=$(cat /etc/modules | grep 'bonding')
 echo $result
 
 if [ "$result" == "" ];then
-    sed -e '/rtc/abonding mode=`bond_mode` miimon=100 max_bonds=4' -i /etc/modules
+    sed '$abonding mode='$BOND_MODE' miimon=100 max_bonds=4' -i /etc/modules
 fi
 
-modprobe bonding mode=$bond_mode miimon=100 max_bonds=4
+echo modprobe bonding mode=$BOND_MODE miimon=100 max_bonds=4
 
-result=$(cat /etc/network/interfaces | grep ${bond_interface})
+modprobe bonding mode=$BOND_MODE miimon=100 max_bonds=4
+
+
+result=$(cat /etc/network/interfaces | grep ${BOND_INTERFACE})
 if [ "$result" == "" ];then
-    /etc/network/interfaces
-    sed auto ${net_interface1}
-    iface ${net_interface1} inet manual
-    bond-master ${bond_interface}
-    
-    auto ${net_interface2}
-    iface ${net_interface2} inet manual
-    bond-master ${bond_interface}
-    
-    auto ${bond_interface} 
-    iface ${bond_interface} inet static
-    address $IP
-    netmask $Netmask
-    gateway $GW
-    post-up ifenslave ${bond_interface} ${net_interface1} ${net_interface2}
-    post-down ifenslave -d bonding ${net_interface1} ${net_interface2}
+    echo auto $NET_INTERFACE1 >> /etc/network/interfaces
+    echo iface $NET_INTERFACE1 inet manual >> /etc/network/interfaces
+    echo bond-master ${BOND_INTERFACE} >> /etc/network/interfaces
+    echo auto $NET_INTERFACE2 >> /etc/network/interfaces
+    echo iface $NET_INTERFACE2 inet manual >> /etc/network/interfaces
+    echo bond-master $BOND_INTERFACE >> /etc/network/interfaces
+    echo auto $BOND_INTERFACE >> /etc/network/interfaces
+    echo iface $BOND_INTERFACE inet static >> /etc/network/interfaces
+    echo address $IP >> /etc/network/interfaces
+    echo netmask $NETMASK >> /etc/network/interfaces
+    echo gateway $GW >> /etc/network/interfaces
+    echo post-up ifenslave $BOND_INTERFACE $NET_INTERFACE1 $NET_INTERFACE2 >> /etc/network/interfaces
+    echo post-down ifenslave -d bonding $NET_INTERFACE1 $NET_INTERFACE2 >> /etc/network/interfaces
 fi
+cat /etc/network/interfaces
 reboot
+
