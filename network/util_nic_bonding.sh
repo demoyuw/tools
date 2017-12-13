0b3360afe23d2ecbd758f6a80b97e64af5ce806e
YuWei Chou <demoyuw@gmail.com>
2017-12-13 18:36:28 +0800

    change parameter

--- network/util_nic_bonding.sh
+++ network/util_nic_bonding.sh
@@ -2,16 +2,17 @@
 # bond NIC from two original nic 
 # 2017-12-11 : Yuwei Chou
 
-if [ $# -lt 4 ] ; then
-    echo "usage : util_nic_bonding.sh bond_nic eth0_nic eth1_nic bond_mode_number"
-    echo "   Eg : util_nic_bonding.sh bond0 eth0 eth1 4"
+if [ $# -lt 5 ] ; then
+    echo "usage : util_nic_bonding.sh bond_nic ip/netmask eth0_nic eth1_nic bond_mode_number"
+    echo "   Eg : util_nic_bonding.sh bond0 192.168.4.202/24 eth0 eth1 4"
     exit -1
 fi
 
 BOND_INTERFACE=$1
-NET_INTERFACE1=$2
-NET_INTERFACE2=$3
-BOND_MODE=$4
+IPNET=$2
+NET_INTERFACE1=$3
+NET_INTERFACE2=$4
+BOND_MODE=$5
 
 ping -c1 -W1 8.8.8.8 &> /dev/null
 if [ "$?" == "0" ]; then
@@ -31,5 +32,5 @@ ip link set $NET_INTERFACE1 down
 ip link set $NET_INTERFACE2 down
 ifenslave $BOND_INTERFACE $NET_INTERFACE1 $NET_INTERFACE2
 ip link set $BOND_INTERFACE up
-#ip addr add 192.168.1.1/24 dev bond0
+ip addr add $IPNET dev $BOND_INTERFACE
 #ip route add default via 192.168.1.254 dev bond0
