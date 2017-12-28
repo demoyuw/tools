082ea0a660b00bce7197c71d23ed377825f27e54
YuWei Chou <demoyuw@gmail.com>
2017-12-28 14:17:53 +0800

    add restart nova service shell

---
+++ nova/restart_nova_service.sh
@@ -0,0 +1,17 @@
+service nova-api status
+service nova-conductor status
+service nova-consoleauth status
+service nova-novncproxy status
+service nova-scheduler status
+
+service nova-api restart
+service nova-conductor restart
+service nova-consoleauth restart
+service nova-novncproxy restart
+service nova-scheduler restart
+
+service nova-api status
+service nova-conductor status
+service nova-consoleauth status
+service nova-novncproxy status
+service nova-scheduler status
