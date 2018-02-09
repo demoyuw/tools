f4b2308ef1cffe0c634514238f7721b407433466
YuWei Chou <demoyuw@gmail.com>
2018-02-09 18:07:00 +0800

    Add snapshot all KVM VM on Ubuntu16

--- kvm/snapshot_all_vm.sh
+++ kvm/snapshot_all_vm.sh
@@ -1,15 +1,13 @@
-LOG_DIR=/home/yuwei/log
-echo "start snapshot" >> ${LOG_DIR}/snapshot_all_vm.log
-date >> ${LOG_DIR}/snapshot_all_vm.log
-virsh snapshot-create isc23CTL1 >> ${LOG_DIR}/snapshot_all_vm.log
-date >> ${LOG_DIR}/snapshot_all_vm.log
-virsh snapshot-create isc23COM1 >> ${LOG_DIR}/snapshot_all_vm.log
-date >> ${LOG_DIR}/snapshot_all_vm.log
-virsh snapshot-create isc23COM2 >> ${LOG_DIR}/snapshot_all_vm.log
-date >> ${LOG_DIR}/snapshot_all_vm.log
-virsh snapshot-create isc2networkCTL >> ${LOG_DIR}/snapshot_all_vm.log
-date >> ${LOG_DIR}/snapshot_all_vm.log
-virsh snapshot-create isc2networkCOM2 >> ${LOG_DIR}/snapshot_all_vm.log
-date >> ${LOG_DIR}/snapshot_all_vm.log
-virsh snapshot-create isc2networkCOM1 >> ${LOG_DIR}/snapshot_all_vm.log
-echo "finish snapshot" >> ${LOG_DIR}/snapshot_all_vm.log
+#!/bin/bash
+
+#create snaphost for each VM
+
+virsh list --all
+# virsh snapshot-list $VMNAME | sed '1,2'd | awk '{print $1}'
+ALLVMS=($(virsh list --all | sed '1,2'd | sed '$d' |awk '{print $2}'))
+
+for (( i=0; i<=((${#ALLVMS[@]}-1)); i++ ));
+do
+    echo snapshot-create ${ALLVMS[i]};
+    virsh snapshot-create ${ALLVMS[i]};
+done
